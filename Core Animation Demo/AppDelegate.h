a06233b04d5df96f6eb3132a12264c1b50477b96
panfeng <panfeng200866@163.com>
2015-07-10 23:06:05 +0800

    Initial Commit

---
+++ Core Animation Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Core Animation Demo
+//
+//  Created by Sammer on 15/7/10.
+//  Copyright (c) 2015年 Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
